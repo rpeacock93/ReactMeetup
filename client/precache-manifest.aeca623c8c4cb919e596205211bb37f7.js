1fedbd6726c604de9133c60f2df731a8f6461a82
rpeacock93 <rpeacock@csumb.edu>
2018-11-12 06:01:59 -0800

    first commit

---
+++ client/precache-manifest.aeca623c8c4cb919e596205211bb37f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6529b77fe1e21e3c6655",
+    "url": "/static/js/main.6529b77f.chunk.js"
+  },
+  {
+    "revision": "266d957133c4b296ebbf",
+    "url": "/static/js/1.266d9571.chunk.js"
+  },
+  {
+    "revision": "6529b77fe1e21e3c6655",
+    "url": "/static/css/main.ce73850b.chunk.css"
+  },
+  {
+    "revision": "ea2fed7874549a4a898dcf410c5d26dd",
+    "url": "/index.html"
+  }
+];
